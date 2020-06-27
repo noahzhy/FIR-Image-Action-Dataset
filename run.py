e047a0f3b3b9b5b5194b30059948cfa9b9000600
noahzhy <noahzhy@gmail.com>
2020-06-27 19:24:18 +0900

    add

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ import board, busio
 import numpy as np
 import pandas as pd
 import adafruit_mlx90640
+import Adafruit_DHT
 
 from IPython import display
 from subprocess import call
@@ -18,18 +19,32 @@ indoor_scene = '01'
 # natural, light, dark
 lighting = 'light'
 # none
-heat_source = 'none'
+heat_source = 'test'
 
 # init the i2c and mlx90640
 i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ
+sensor = Adafruit_DHT.DHT22
+gpio = 17
+_start = time.time()
+
+
+def get_temperature(gpio=17):
+    try:
+        _, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+    except Exception as e:
+        return 0
+    return round(temperature, 2)
+
+
+# t = get_temperature()
 
 # init csv file
 file_create_time = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 file_header = ['Time', 'RT'] + ['P{:03d}'.format(i) for i in range(768)]
-csv_file_name = "{}_mlx90640_{}_{}_{}.csv".format(file_create_time, indoor_scene, lighting, heat_source)
-
+base_file_name = "{}_mlx90640_{}_{}_{}".format(file_create_time, indoor_scene, lighting, heat_source)
+csv_file_name = "{}.csv".format(base_file_name)
 # init dataframe header
 # df = pd.DataFrame(columns=file_header)
 # df.to_csv(csv_file_name, index=False, header=file_header)
@@ -41,19 +56,19 @@ camera.resolution = (320, 240)
 camera.framerate = 24
 camera.vflip = True
 
-camera.start_recording('{}.h264'.format(file_create_time))
+camera.start_recording('{}.h264'.format(base_file_name))
 
-_start = time.time()
+zero = time.time()
 data = list()
 
 try:
     while True:
-        try:
+        keep_time = time.time() - zero
 
+        try:
             local_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-2]
             _start = time.time()
             mlx.getFrame(frame)
-
         except ValueError as e:
             print('ValueError', e)
             continue
@@ -61,7 +76,7 @@ try:
         res = np.around(frame, 2).tolist()
         data.append([local_time, 0] + res)
 
-        print(1 / (time.time() - _start))
+        print('FPS:', round((1 / (time.time() - _start)), 2), 'time:', round(keep_time, 2))
 
 except KeyboardInterrupt as e:
     print('KeyboardInterrupt', e)
@@ -70,5 +85,5 @@ except KeyboardInterrupt as e:
     camera.stop_recording()
     camera.close()
 
-convert = "MP4Box -add {}.h264 {}.mp4".format(file_create_time, file_create_time) # defining convert
-call ([convert], shell=True) # executing convert using call
+convert = "MP4Box -add {}.h264 {}.mp4".format(base_file_name, base_file_name)
+call([convert], shell=True)
